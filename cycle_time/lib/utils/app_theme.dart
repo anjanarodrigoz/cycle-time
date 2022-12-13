a364b7dcdb3a668e27a462dcc075ed7ad16e4602
anjanarodrigoz <85009096+anjanarodrigoz@users.noreply.github.com>
2022-12-13 12:56:30 +0530

    add colors

---
+++ cycle_time/lib/utils/app_theme.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+
+class AppSize {}
+
+class AppColor {
+  static const green = Color(0xFF00D67C);
+  static const yellow = Color(0xFFE8BB1B);
+  static const blue = Color(0xFF1BC3E8);
+  static const purple = Color(0xFF9A1BE8);
+  static const pink = Color(0xFFE81B7D);
+  static const red = Color(0xFFE81B1B);
+  static const greenOne = Color(0xFF12DE00);
+  static const greenTwo = Color(0xFF16947D);
+}
